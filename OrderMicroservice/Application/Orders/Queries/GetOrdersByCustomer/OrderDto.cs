6355a922ad9a1222cf17a214ce0222f0e091ea21
I PUTU ANANDA DN <ianandad@fmi.com>
2021-11-20 09:42:41 +0700

    [OrderMicroservice]: add cancellation reason on OrderDto

--- OrderMicroservice/Application/Orders/Queries/GetOrdersByCustomer/OrderDto.cs
+++ OrderMicroservice/Application/Orders/Queries/GetOrdersByCustomer/OrderDto.cs
@@ -23,6 +23,7 @@ namespace Application.Orders.Queries.GetOrdersByCustomer
         public List<OrderItemDto> Items { get; set; }
         public OrderStatus Status { get; set; }
         public PaymentStatus PaymentStatus { get; set; }
+        public string CancellationReason { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
         public DateTime? ServedOn { get; set; }
